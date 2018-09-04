5614fafe7ff1d480c5b8db16edd0a9883c9d6827
avell21 <abeltyr@gmail.com>
2018-09-04 18:45:46 +0300

    controller done pagination here we go

---
+++ Controllers/BlogController.js
@@ -0,0 +1,130 @@
+/* this get the index from model and later is used
+ *  to find the blog model   
+ */
+const db = require('../models');
+
+
+// this is the controller to list all
+
+exports.list_all = function (req, res) {
+    var limit = 0; // number of records per page
+    var offset = 1;
+    db.blog.findAndCountAll()
+        .then((data) => {
+            res.json(["data", data, ]);
+        })
+        .catch(function (error) {
+            res.status(500).send('Internal Server Error');
+        });
+};
+
+/* this is the controller to list all blog
+ on a specific  categoric and the amount of those blogs
+*/
+exports.list_Category = function (req, res) {
+    db.blog
+        .findAndCountAll({
+            where: {
+                category: req.params.category
+            }
+        }).then(result => {
+            res.json(["data", result]);
+        })
+        .catch(function (error) {
+            res.status(500).send('Internal Server Error');
+        });
+};
+
+/* this is the controller to list all blog
+ based on title and the amount of those blogs
+*/
+exports.list_Title = function (req, res) {
+    db.blog
+        .findAndCountAll({
+            where: {
+                title: req.params.title
+            }
+        })
+        .then(result => {
+            res.json(["data", result]);
+        });
+};
+
+/* this is the controller to list all blog based on 
+ *  specific  user and the amount of those blogs
+ */
+
+exports.blog_User = function (req, res) {
+    db.blog.findAndCountAll({
+        where: {
+            user_id: req.params.user
+        }
+    }).then(result => {
+        res.json(["data", result]);
+    });
+};
+
+/**create a new blog if it went well the created blog 
+ * data is send back if there are error like there being empty
+ * data being send it shows an error
+ */
+
+exports.New_blog = function (req, res) {
+    db.blog
+        .create({
+            user_id: req.body.user_id,
+            title: req.body.title,
+            category: req.body.category,
+            content: req.body.content
+        })
+        .then(result => {
+            res.json(result);
+        })
+        .catch(error => {
+            res.json(error.errors);
+        });
+};
+
+/**Update a blog by finding thr blog by idif it went well the created blog 
+ * data is send back if there are error like there being empty
+ * data being send it shows an error
+ */
+
+exports.Update_blog = function (req, res) {
+    db.blog
+        .update({
+            title: req.body.title,
+            category: req.body.category,
+            content: req.body.content
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(result => {
+            res.json(result);
+        })
+        .catch(error => {
+            res.json(error);
+        });
+};
+
+/**Delete a blog by finding thr blog by idif it went well the created blog 
+ * data is send back if there are error like there being empty
+ * data being send it shows an error
+ */
+
+exports.Delete_blog = function (req, res) {
+    db.blog
+        .destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(result => {
+            res.json(result);
+        })
+        .catch(error => {
+            res.json(error);
+        });
+};
