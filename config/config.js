c3ea9292d2f39c066977ba286137f920019fe47e
avell21 <abeltyr@gmail.com>
2018-09-22 06:14:00 +0300

    do

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_CONNECTION
+    "dialect": "mysql"
   },
   "env": {
     "FACEBOOK_APP_ID": "2265600380390466",
