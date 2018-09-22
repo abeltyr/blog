3b5a45fa4f7f640ebd59d6fbde7d6628c9e9d94a
avell21 <abeltyr@gmail.com>
2018-09-22 05:03:16 +0300

    do

--- models/blog.js
+++ models/blog.js
@@ -1,12 +1,14 @@
 'use strict';
+const comment = require("./comment")
 module.exports = (sequelize, DataTypes) => {
+  const comment = sequelize.import(__dirname + "/comment")
   const blog = sequelize.define('blog', {
     id: {
-        allowNull: false,
-        autoIncrement: false,
-        primaryKey: true,
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      autoIncrement: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
     },
     user_id: {
       type: DataTypes.UUID,
@@ -37,8 +39,17 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
   }, {});
-  blog.associate = function (models) {
+  blog.associate = function (comment) {
     // associations can be defined here
+    blog.hasMany(comment, {
+      foreignKey: 'blog_id',
+      sourceKey: 'id'
+    })
+    comment.belongsTo(blog, {
+      foreignKey: 'blog_id',
+      targetKey: 'id'
+    });
   };
+
   return blog;
 };
