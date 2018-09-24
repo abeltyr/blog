3e42c33934f93f216f5b0c279d039e3bb2394d51
avell21 <abeltyr@gmail.com>
2018-09-24 06:19:51 +0300

    as

--- config/config.js
+++ config/config.js
@@ -13,18 +13,5 @@ module.exports = {
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
-  },
-  "production": {
-    "username": "mikiast",
-    "password": "ethiopia",
-    "database": "blogapp",
-    "host": "db4free.net",
-    "dialect": "mysql"
-  },
-  "env": {
-    "FACEBOOK_APP_ID": "2265600380390466",
-    "FACEBOOK_APP_SECRET": "9722f2175eefa00e960252205cc7c5a7",
-    "GOOGLE_APP_ID": "585886034796-4p4jjpivhd07iklttd6tgqmmk79v2opu.apps.googleusercontent.com",
-    "GOOGLE_APP_SECRET": "LdFDTQFP-yBTYzoSFpyKtUZ2"
   }
 }
